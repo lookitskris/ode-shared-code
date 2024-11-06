3df025a91508af47439baf0b4248ace8ba0ea008
Kris Athi <kris@onedoteleven.com>
2024-11-06 15:14:11 +0000

    Added module files and svg resources

---
+++ dist/assets/svg/index.d.ts
@@ -0,0 +1,4 @@
+import ODELogo from './onedoteleven-logo.svg';
+import ODETitle from './onedoteleven-title.svg';
+import MickyMouse from './mickey-mouse.svg';
+export { ODELogo, ODETitle, MickyMouse };
---
+++ src/assets/svg/index.ts
@@ -0,0 +1,6 @@
+import ODELogo from './onedoteleven-logo.svg';
+import ODETitle from './onedoteleven-title.svg';
+import MickyMouse from './mickey-mouse.svg';
+
+
+export { ODELogo, ODETitle, MickyMouse };
